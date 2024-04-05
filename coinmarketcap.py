5876b985ac35e63f9fa8af9d12594b9d238bd233
davidgaba <davidgaba03@gmail.com>
2024-04-05 10:19:55 -0700

    test

---
+++ coinmarketcap.py
@@ -0,0 +1,44 @@
+from secret_key import API_KEY
+import requests
+from requests import Session
+from pprint import pprint as pp
+
+
+class CoinMarketCap:
+    # "https://coinmarketcap.com/api/documentation/v1/#"
+    def __init__(self, token):
+        self.api_url = "https://pro-api.coinmarketcap.com"
+        self.headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': token,
+        }
+        self.session = Session()
+        self.session.headers.update(self.headers)
+
+    def get_coins(self):
+        url = self.api_url + "/v1/cryptocurrency/map"
+        r = self.session.get(url)
+        data = r.json()['data']
+
+        return data
+
+
+    def get_price(self, symbol):
+        url = self.api_url + "/v2/cryptocurrency/quotes/latest"
+        parameters = {'symbol': symbol}
+        r = self.session.get(url, params=parameters)
+        data = r.json()['data']
+
+        return data
+
+
+def main():
+    cmc = CoinMarketCap(API_KEY)
+
+    # pp(cmc.get_coins())
+    pp(cmc.get_price('BTC'))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ secret_key.py
@@ -0,0 +1,1 @@
+API_KEY = 'ccbe9bf1-8c11-4ba4-86f9-fc0aac0ca541'
